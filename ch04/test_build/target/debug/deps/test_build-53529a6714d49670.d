9aebb47785be71a35924d2ca854503c7946e9bc7
Perlverity <snowman.ss.6124@gmail.com>
2023-12-27 21:31:54 +0900

    chapter 04-03 done.

---
+++ ch04/test_build/target/debug/deps/test_build-53529a6714d49670.d
@@ -0,0 +1,5 @@
+/Users/administrator/Rust/Rust/ch04/test_build/target/debug/deps/test_build-53529a6714d49670: src/main.rs
+
+/Users/administrator/Rust/Rust/ch04/test_build/target/debug/deps/test_build-53529a6714d49670.d: src/main.rs
+
+src/main.rs:
---
+++ ch04/trait_TreasureBox/target/debug/deps/trait_TreasureBox-f7cdae2aff420aea.d
@@ -0,0 +1,5 @@
+/Users/administrator/Rust/Rust/ch04/trait_TreasureBox/target/debug/deps/trait_TreasureBox-f7cdae2aff420aea: src/main.rs
+
+/Users/administrator/Rust/Rust/ch04/trait_TreasureBox/target/debug/deps/trait_TreasureBox-f7cdae2aff420aea.d: src/main.rs
+
+src/main.rs:
