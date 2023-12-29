4c2b4c08df0091ffcfbb18039fe3717265de8425
Perlverity <snowman.ss.6124@gmail.com>
2023-12-29 15:03:37 +0900

    chapter 04 done.

---
+++ ch04/rpn_calc/target/package/rpn_calc_perl-0.1.1/target/debug/deps/rpn_calc-5ffea2b6d89bb19a.d
@@ -0,0 +1,5 @@
+/home/s-suganuma/rust-book/ch04/rpn_calc/target/debug/deps/rpn_calc-5ffea2b6d89bb19a: src/lib.rs
+
+/home/s-suganuma/rust-book/ch04/rpn_calc/target/debug/deps/rpn_calc-5ffea2b6d89bb19a.d: src/lib.rs
+
+src/lib.rs:
